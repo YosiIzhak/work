4151178d8f7cca87862db6a7cfc3ea1f6ad8e4fc
YosiIzhak <77921481+YosiIzhak@users.noreply.github.com>
2022-02-17 17:02:20 +0200

    all

--- inc/ball.hpp
+++ inc/ball.hpp
@@ -10,19 +10,19 @@ class Ball
 {
 public:
     Ball();
-    explicit Ball(const char* a_color, int a_radius = 1);
+    explicit Ball(char a_color = 'w', int a_radius = 1);
     int getRadius()const;
     void setRadius(int a_radius);
     const char* getColor() const;
     void swap(Ball& a_other);
 private:
-    char m_color[10];
+    char m_color;
     int m_radius;
 };
 
 bool operator<(Ball const& a_first, Ball const& a_second);
 void swap(Ball& a_first, Ball& a_second);
 } //cpp namespace
-
+#include "./inl/ball.hxx"
 
 #endif /*#ifndefBALL_H122*/
---
+++ inc/inl/ball.hxx
@@ -0,0 +1,55 @@
+#include <cstddef>
+
+#include "ball.hpp"
+
+namespace cpp
+{
+
+
+Ball::Ball(char a_color, int a_radius)
+:m_color(a_color)
+, m_radius(a_radius)
+{
+}
+
+int Ball::getRadius()const
+{
+    return m_radius;
+}
+
+void Ball::setRadius(int a_radius)
+{
+    m_radius = a_radius;
+}
+
+bool operator==(Ball const& a_first, Ball const& a_second)
+{
+	return a_first.getRadius() == a_second.getRadius();
+}
+
+bool operator!=(Ball const& a_first, Ball const& a_second)
+{
+	return !(a_first == a_second);
+}
+
+bool operator<(Ball const& a_first, Ball const& a_second)
+{
+    return a_first.getRadius() - a_second.getRadius() < 0;
+}
+
+bool operator>(Ball const& a_first, Ball const& a_second)
+{
+	return !(a_first < a_second);
+}
+
+bool operator>=(Ball const& a_first, Ball const& a_second)
+{
+	return (a_first > a_second) || (a_first == a_second);
+}
+
+bool operator<=(Ball const& a_first, Ball const& a_second)
+{
+	return (a_first < a_second) || (a_first == a_second);
+}
+
+} //cpp namespace
