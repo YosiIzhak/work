e0e04cc4e3b6983e387e9ae22ed62a527e50dddf
YosiIzhak <77921481+YosiIzhak@users.noreply.github.com>
2022-02-27 20:32:16 +0200

    sorted test

--- test/sorted_containers/utest.cpp
+++ test/sorted_containers/utest.cpp
@@ -2,13 +2,15 @@
 #include "sorted_container.hpp"
 #include "sorted_list.hpp"
 #include "sorted_vector.hpp"
-
+#include "sorted_unique_vector.hpp"
+#include "sorted_unique_list.hpp"
 
 BEGIN_TEST(vec_new)
-	// using namespace cpp;
-	// SortedVector vec(10);
-	// ASSERT_EQUAL(vec.size(), 10);
-	std::cout << "aaa";
+	using namespace cpp;
+	SortedVector<int> vec(10);
+	ASSERT_EQUAL(vec.size(), 0);
+	vec.insert(7);
+	ASSERT_EQUAL(vec.size(), 1);
 	ASSERT_PASS();
 	
 END_TEST
