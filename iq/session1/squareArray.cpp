afffd3f9b3abf98fac8caa6898df9739f6d1f0e6
YosiIzhak <77921481+YosiIzhak@users.noreply.github.com>
2022-03-29 21:51:41 +0300

    iq

---
+++ iq/session1/squareArray.cpp
@@ -0,0 +1,70 @@
+#include<iostream>   
+#include<algorithm>   
+#include<vector>  
+#include <stdlib.h> 
+#include <cmath>
+
+using namespace std;   
+
+void squareArray(vector<int>& a_numbers)
+{
+    auto begin = a_numbers.begin();
+    auto end = a_numbers.end();
+    auto isPositive = [](int a_num){return a_num >= 0;};
+    auto pivot = std::find_if(begin, end, isPositive);
+    auto pow2 = [](int& a_num){a_num =  a_num*a_num;};
+ 
+    if(pivot == begin)
+    {
+        for_each(begin, end, pow2);
+        return;
+    }
+    reverse(begin, pivot);
+    for_each(begin, end, pow2);
+    inplace_merge(begin, pivot, end);
+}
+ 
+int main()
+{
+    vector<int> vec = { -6, -3, -1, 2, 4, 5 };
+    int n = vec.size();
+ 
+    cout << "Before sort " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << vec[i] << " ";
+    }
+        
+    squareArray(vec);
+ 
+    cout << "\nAfter Sort " << endl;
+    for (int i = 0; i < n; i++)
+    {
+         cout << vec[i] << " ";
+    }
+       cout << endl;
+ 
+    return 0;
+}
+
+
+// int main () {   
+//   const int N = 100;
+//   array<long, N> array; 
+  
+//   for(size_t i = 0; i < N; ++i)
+//   {
+//       array[i] = i;
+//   }
+//   squareArray(array);
+    
+//     for (int i = 0; i < N; ++i)
+//     {
+//           cout << array[i] << " ";
+//     }
+//     cout << endl;
+   
+    
+// return 0;   
+// }   
+//auto isPositive = [](int a_num){return a_num >= 0;};
