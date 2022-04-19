462713f5cbe90a982cd4c83a6bd352c962c688bb
YosiIzhak <77921481+YosiIzhak@users.noreply.github.com>
2022-04-19 09:27:14 +0300

    iq

---
+++ iq/session1/pairSum.cpp
@@ -0,0 +1,33 @@
+#include <iostream>    
+#include <algorithm> 
+#include <array>
+#include <unordered_map>
+
+using namespace std;
+
+template <size_t N>
+int pairSum(array<int, N>& a_array, int a_number)
+{
+    unordered_map<int, size_t> map;
+    size_t counter = 0;
+    
+    for (auto e : a_array)
+    {
+        int delta = a_number - e;
+        if(map.find(delta) != map.end())
+        {
+            counter += map[delta];
+        }
+        ++map[e];
+    }
+    return counter;
+}
+
+int main () 
+{
+   array<int, 10> myArray = {1,2,3,4,5,6,7,8,9,10};
+   int res = pairSum(myArray, 17);
+    cout << "res " << res << endl;
+
+    return 0;
+}
